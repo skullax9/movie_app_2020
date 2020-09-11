2cfcd7ae4db35a9af5106f2b86812432af73d7ab
skullax9 <bunjob9936@gmail.com>
2020-09-11 11:38:45 +0900

    Edit App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React from 'react';
 
-function Food() {
-  return <h1>I love Potato</h1>
-}
-
 function App() {
-  return (
-    <div>
-      <h1>Hell0</h1>
-      <Food name = "kimchi" />
-      </div>
-    
-  )
+  return <div className="App"></div>;
 }
 
 export default App;
