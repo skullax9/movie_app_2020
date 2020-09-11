bdff75db4ccb197b110cc1e469a55a6c078c32c7
skullax9 <bunjob9936@gmail.com>
2020-09-11 12:38:30 +0900

    App.js 수정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 function App() {
-  return <div className="App"></div>;
+ // return <div className="App"/>
+    return (
+      <div>
+        <h1>
+          Hello??</h1>
+      </div>
+      );
 }
 
 export default App;
