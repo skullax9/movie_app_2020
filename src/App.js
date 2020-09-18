1bbfe8d421d80d84f93c524366ae93bdc1f317d5
skullax9 <bunjob9936@gmail.com>
2020-09-18 12:48:40 +0900

    컴포넌트와 JSX학습

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
 
+function Food({fav}) {
+  return <h1>I LOVE {fav}</h1>;
+}
+
+const foodlike = [
+  {
+    name: '김치',
+  },
+  {
+    name: '만두',
+  },
+  {
+    name: '피자',
+  },
+];
+
 function App() {
  // return <div className="App"/>
-    return (
-      <div>
-        <h1>
-          Hello??</h1>
-      </div>
-      );
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+    );
 }
 
 export default App;
