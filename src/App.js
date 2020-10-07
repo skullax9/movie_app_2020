fa2323fc92cb882ddca9ce5c1dd4d02334e338ad
skullax9 <bunjob9936@gmail.com>
2020-10-07 09:58:34 +0900

    리액트 5주차 실습

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
+import img1 from './images/kimchi.jpg';
+import img2 from './images/dongatsu.jpg';
+import img3 from './images/sushi.jpg';
+import img4 from './images/bibimbap.jpg';
+import img5 from './images/ddeok.jpg';
 
-function Food({fav}) {
-  return <h1>I LOVE {fav}</h1>;
+function Food({name,picture,message}) {
+  return (
+  <div><h2>I love {name}</h2>
+  <img src = {picture} alt={message}/>
+  </div>)
 }
 
-const foodlike = [
+const foodILike = [
   {
-    name: '김치',
+    id: 1,
+    name: '김치찌개',
+    image: img1,
+    message: '김치찌개 사진'
   },
   {
-    name: '만두',
+    id: 2,
+    name: '돈가츠',
+    image: img2,
+    message: '돈가츠 사진'
   },
   {
-    name: '피자',
+    id: 3,
+    name: '스시',
+    image: img3,
+    message: '스시 사진'
+  },
+  {
+    id: 4,
+    name: '비빔밥',
+    image: img4,
+    message: '비빔밥 사진'
+  },
+  {
+    id: 5,
+    name: '떡볶이',
+    image: img5,
+    message: '떡볶이 사진'
   },
 ];
 
+function renderFood(dish) {
+  return <Food id={dish.id} name={dish.name} picture={dish.image} alt={dish.message}/>
+}
+
 function App() {
- // return <div className="App"/>
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>{foodILike.map(renderFood)}</h1>
     </div>
     );
 }
