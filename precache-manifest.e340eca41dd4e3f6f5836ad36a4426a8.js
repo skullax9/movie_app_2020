928bb0bff47b6d2c253f5e820b034a537f95479a
skullax9 <skullax9@naver.com>
2020-11-20 12:36:49 +0900

    Updates

---
+++ precache-manifest.e340eca41dd4e3f6f5836ad36a4426a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7df536347e175d97b5427116eb9795",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "1521315afb08b876c9db",
+    "url": "/movie_app_2020/static/css/main.71ae580f.chunk.css"
+  },
+  {
+    "revision": "344f48a0d3551cb38225",
+    "url": "/movie_app_2020/static/js/2.1fa7d33f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/movie_app_2020/static/js/2.1fa7d33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1521315afb08b876c9db",
+    "url": "/movie_app_2020/static/js/main.b4026b7b.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
